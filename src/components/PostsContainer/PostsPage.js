b4d1e625674c8bd4d50ec2db0a375d1e531ce1dd
Amber Chunn <a.77@me.com>
2020-03-03 15:56:11 -0800

    Added props to LikeSection

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,31 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
-import Post from "./Post";
-import "./Posts.css";
-// import data 
+import React, { useState } from 'react';
+import Post from './Post';
+import './Posts.css';
 
-const PostsPage = () => {
-  // set up state for your data
-  return (
-    <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
-    </div>
-  );
+// import data
+import dummyData from '../../dummy-data';
+
+const PostsPage = props => {
+	// set up state for your data
+	const [posts, setPosts] = useState();
+	return (
+		<div className="posts-container-wrapper">
+			{dummyData.map(post => {
+				return (
+					<Post
+						username={props.post.username}
+						// thumbnailUrl={props.post.thumbnailUrl}
+						// imageUrl={props.post.imageUrl}
+						// likes={props.post.likes}
+					/>
+				);
+			})}
+		</div>
+	);
 };
 
 export default PostsPage;
 
+// map through data here to return a Post and pass data as props to Post
